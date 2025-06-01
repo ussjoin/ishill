2c5403c896cf396dfb391ec04f0cfbd8231f3afb
Brendan Francis O'Connor <ussjoin@ussjoin.com>
2025-06-01 13:46:15 -0700

    OK, try without the pipe I guess.

--- layouts/index.zineify.sh
+++ layouts/index.zineify.sh
@@ -5,6 +5,8 @@ set -euxo pipefail
 
 vivliostyle build
 {{ range $pages }}
-    psbook '.{{ .Path }}/{{- .Title | urlize -}}.view.pdf' | psnup -2 -p letter > '.{{ .Path }}/{{- .Title | urlize -}}.zine.pdf'
+    psbook '.{{ .Path }}/{{- .Title | urlize -}}.view.pdf' > '.{{ .Path }}/{{- .Title | urlize -}}.tmp.pdf'
+    psnup -2 -p letter '.{{ .Path }}/{{- .Title | urlize -}}.tmp.pdf' > '.{{ .Path }}/{{- .Title | urlize -}}.zine.pdf'
+    rm '.{{ .Path }}/{{- .Title | urlize -}}.tmp.pdf'
 {{ end }}
 
