a2f95f0dbbfee4b79674309cceb89b526e349a18
Brendan Francis O'Connor <ussjoin@ussjoin.com>
2025-06-01 13:41:56 -0700

    Adding pipefail to the zineify script to track down bookmaking issue.

--- layouts/index.zineify.sh
+++ layouts/index.zineify.sh
@@ -1,11 +1,10 @@
 #!env bash
+set -euxo pipefail
 
 {{ $pages := where .Site.RegularPages "Type" "in" .Site.Params.mainSections }}
 
 vivliostyle build
 {{ range $pages }}
-    INNAME='.{{ .Path }}/{{- .Title | urlize -}}.view.pdf'
-    OUTNAME='.{{ .Path }}/{{- .Title | urlize -}}.zine.pdf'
-    psbook $INNAME | psnup -2 -p letter > $OUTNAME
+    psbook '.{{ .Path }}/{{- .Title | urlize -}}.view.pdf' | psnup -2 -p letter > '.{{ .Path }}/{{- .Title | urlize -}}.zine.pdf'
 {{ end }}
 
