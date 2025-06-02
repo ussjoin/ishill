bcb7f495ebb884f4543145221a6ca655d39a8cb0
Brendan Francis O'Connor <ussjoin@ussjoin.com>
2025-06-02 12:00:16 -0700

    Trying pdfimpose instead of psnup.

--- layouts/index.zineify.sh
+++ layouts/index.zineify.sh
@@ -5,8 +5,6 @@ set -euxo pipefail
 
 vivliostyle build
 {{ range $pages }}
-    psbook '.{{ .Path }}/{{- .Title | urlize -}}.view.pdf' > '.{{ .Path }}/{{- .Title | urlize -}}.tmp.pdf'
-    psnup -2 -p letter '.{{ .Path }}/{{- .Title | urlize -}}.tmp.pdf' > '.{{ .Path }}/{{- .Title | urlize -}}.zine.pdf'
-    rm '.{{ .Path }}/{{- .Title | urlize -}}.tmp.pdf'
+    pdfimpose saddle -f letter -o  '.{{ .Path }}/{{- .Title | urlize -}}.zine.pdf' '.{{ .Path }}/{{- .Title | urlize -}}.view.pdf'
 {{ end }}
 
