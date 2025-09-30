a34cc399c21d1ffb0d9d15ea17195cba932704b4
Brendan Francis O'Connor <ussjoin@ussjoin.com>
2025-09-30 16:34:06 -0700

    Moving to Weasyprint from Vivliostyle.

--- layouts/index.zineify.sh
+++ layouts/index.zineify.sh
@@ -1,12 +1,26 @@
 #!env bash
 set -euxo pipefail
 
+function cleanup()
+{
+    # Terminates http.server
+    kill $(cat PID)
+    rm PID
+}
+
+trap cleanup EXIT
+
+python3 -m http.server & # starts on port 8000
+# $! == PID of last background process
+echo $! > PID
+
 {{ $pages := where .Site.RegularPages "Type" "in" .Site.Params.mainSections }}
 
-vivliostyle build
 {{ range $pages }}
-    {{- if ne "imprints" .Type -}}
+    weasyprint  http://localhost:8000{{ .Path }}/  .{{ .Path }}/{{- .Title | urlize -}}.view.pdf
+    {{ if ne "imprints" .Type }}
         pdfimpose saddle -f letter -o  '.{{ .Path }}/{{- .Title | urlize -}}.zine.pdf' '.{{ .Path }}/{{- .Title | urlize -}}.view.pdf'
     {{ end }}
 {{ end }}
+
 
